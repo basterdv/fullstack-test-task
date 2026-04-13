aa82255884fbbf663357871a135b1495fbbd3dd7
Василий Ланшаков ( Vasiliy Lanshakov) <130412972+basterdv@users.noreply.github.com>
2026-04-13 23:15:28 +1000

    Delete backend directory

--- backend/src/api/router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from src.api.v1.files import router as files_router
-from src.api.v1.alerts import router as alerts_router
-
-api_router = APIRouter(prefix="")
-api_router.include_router(files_router)
-api_router.include_router(alerts_router)
